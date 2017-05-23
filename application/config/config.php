d4258e4d53187a19067bc6273c8d710f7b8e7ab6
Bigetion <adysuprianto@yahoo.co.id>
2017-05-24 00:50:39 +0700

    Add Config

---
+++ application/config/config.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+	'base_url'						=> 'http://localhost/kAPI/',
+	'default_app_method'			=> 'index',
+	'default_project'				=> 'page',
+	'default_project_controller'	=> 'data',
+	'default_project_method'		=> 'index',
+	'secret_key'					=> '123456789'
+)
+?>
